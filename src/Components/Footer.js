fddc7720034110a05ea1624dbfa41892747ffd30
Roman Novosad <novosad.r@nimses.com>
2019-05-14 01:20:59 +0300

    replace all the texts with my own

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,16 +19,6 @@ class Footer extends Component {
         <div className="row">
           <div className="twelve columns">
             <ul className="social-links">{networks}</ul>
-
-            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
-              <li>
-                Design by{' '}
-                <a title="Styleshout" href="http://www.styleshout.com/">
-                  Styleshout
-                </a>
-              </li>
-            </ul>
           </div>
           <div id="go-top">
             <a className="smoothscroll" title="Back to Top" href="#home">
