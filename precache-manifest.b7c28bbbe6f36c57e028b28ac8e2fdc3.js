860ba37b53f3cf2df37fcbddd31a2f98ef72eccf
Roman Novosad <romek.novosad@gmail.com>
2019-05-31 13:10:20 +0300

    Updates

---
+++ precache-manifest.b7c28bbbe6f36c57e028b28ac8e2fdc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1c353d184bffa4dc1b2fb4d80652be",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "5e63921a1140c8456b65",
+    "url": "/cv/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "17668d01afca0d313a68",
+    "url": "/cv/static/js/2.1ab6adf9.chunk.js"
+  },
+  {
+    "revision": "5e63921a1140c8456b65",
+    "url": "/cv/static/js/main.5c62cbad.chunk.js"
+  },
+  {
+    "revision": "18079a4bdaee1d56978c",
+    "url": "/cv/static/js/runtime~main.6f651ed7.js"
+  }
+]);
